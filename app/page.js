c2015b65b20a8e626d2589bd123dd9b9afd4e07e
Shane <shane.bergman@corteva.com>
2023-08-21 13:14:56 -0600

    add sidebar

--- app/page.js
+++ app/page.js
@@ -1,13 +1,21 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import TopCards from "./components/TopCards";
+import BarChart from "./components/BarChart";
+import RecentOrders from "./components/RecentOrders";
 export default function Home() {
   return (
     <>
       <Sidebar>
         <main className="bg-gray-100 min-h-screen">
-          <Header></Header>
+          <Header />
+          <TopCards />
+          <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
+            <BarChart />
+            <RecentOrders />
+          </div>
         </main>
       </Sidebar>
     </>
