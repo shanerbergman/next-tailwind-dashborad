b69f5f1e254cee6c9eec19de2ba029669474417f
Shane <shane.bergman@corteva.com>
2023-08-21 21:54:44 -0600

    add customers

--- app/page.js
+++ app/page.js
@@ -1,23 +1,19 @@
 import Image from "next/image";
 
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
 import TopCards from "./components/TopCards";
 import BarChart from "./components/BarChart";
 import RecentOrders from "./components/RecentOrders";
+
 export default function Home() {
   return (
-    <>
-      <Sidebar>
-        <main className="bg-gray-100 min-h-screen">
-          <Header />
-          <TopCards />
-          <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
-            <BarChart />
-            <RecentOrders />
-          </div>
-        </main>
-      </Sidebar>
-    </>
+    <main className="bg-gray-100 min-h-screen">
+      <Header />
+      <TopCards />
+      <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
+        <BarChart />
+        <RecentOrders />
+      </div>
+    </main>
   );
 }
